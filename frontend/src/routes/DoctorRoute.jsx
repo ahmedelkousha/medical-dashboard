8414a1cf50a0c169034f7d646705154d9e3e8d35
codexpro410 <codex410@gmail.com>
2025-12-13 06:49:05 +0200

    fix:disable protected routes temporarily

--- frontend/src/routes/DoctorRoute.jsx
+++ frontend/src/routes/DoctorRoute.jsx
@@ -2,6 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../context/AuthProvider";
 
 export default function DoctorRoute() {
-  const { user } = useAuth();
-  return user?.role === "doctor" ? <Outlet /> : <Navigate to="/login" />;
+  // const { user } = useAuth();
+  // return user?.role === "doctor" ? <Outlet /> : <Navigate to="/login" />;
+    return <Outlet /> ;
 }
